f53cca8465612521c8e1d5a218604b867391d79d
Jinho Heo <uj07077@gmail.com>
2023-04-22 17:54:45 +0900

    add cpp_module_02 ex03, debug needed

--- 4/cpp_module/02/ex03/bsp.cpp
+++ 4/cpp_module/02/ex03/bsp.cpp
@@ -1,15 +1,27 @@
 #include "Point.hpp"
 
-Vec operator-(Point const &a, Point const &b)
-{
-	return Vec(a.getX() - b.getX(), a.getY() - b.getY());
+Vec operator-(Point const &a, Point const &b) {
+  return Vec(a.getX().toFloat() - b.getX().toFloat(),
+             a.getY().toFloat() - b.getY().toFloat());
 }
 
-bool bsp( Point const a, Point const b, Point const c, Point const point)
-{
-	Vec ab(b - a);
-	Vec ac(c - a);
-	Vec ap(point - a);
+Vec operator*(Vec const &a, Vec const &b) {
+  return Vec(a.getX().toFloat() * b.getX().toFloat(),
+             a.getY().toFloat() * b.getY().toFloat());
+}
 
-	
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+  Vec ab(b - a);
+  Vec ac(c - a);
+  Vec ap(point - a);
+
+  Vec u = ab * ap;
+  Vec v = ac * ap;
+
+  float ratio = u.getX().toFloat() / ab.getX().toFloat() +
+                v.getX().toFloat() / ac.getX().toFloat();
+
+  if (ratio > 1 || ratio < 0)
+    return false;
+  return true;
 }
