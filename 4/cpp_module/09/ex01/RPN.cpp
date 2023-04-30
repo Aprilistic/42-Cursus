5d4c2e656e942de488bb171db5b48574dc9a27ec
Jinho Heo <uj07077@gmail.com>
2023-04-30 18:41:59 +0900

    update cpp module 09

--- 4/cpp_module/09/ex01/RPN.cpp
+++ 4/cpp_module/09/ex01/RPN.cpp
@@ -1,4 +1,5 @@
 #include "RPN.hpp"
+#include <cctype>
 
 RPN::RPN() {}
 
@@ -15,35 +16,51 @@ int RPN::evaluateRPN(const std::string &str) {
   std::stack<int> s;
   std::stringstream ss(str);
 
-  std::string token;
-  while (ss >> token) {
-    if (isdigit(token[0])) {
-      s.push(stod(token));
-    } else if (token == "+" || token == "-" || token == "*" || token == "/") {
+  char token;
+  while (true) {
+    ss >> token;
+    if (ss.eof()) {
+      break;
+    } else if (ss.fail()) {
+      throw std::exception();
+    }
+
+    if (std::isspace(token)) {
+      continue;
+    } else if (isdigit(token)) {
+      s.push(token - '0');
+    } else if (token == '+' || token == '-' || token == '*' || token == '/') {
+      if (s.size() < 2) {
+        throw std::exception();
+      }
       int op2 = s.top();
       s.pop();
       int op1 = s.top();
       s.pop();
 
-      if (token == "+") {
+      switch (token) {
+      case '+':
         s.push(op1 + op2);
-      } else if (token == "-") {
+        break;
+      case '-':
         s.push(op1 - op2);
-      } else if (token == "*") {
+        break;
+      case '*':
         s.push(op1 * op2);
-      } else if (token == "/") {
+        break;
+      case '/':
+        if (op2 == 0) {
+          throw std::exception();
+        }
         s.push(op1 / op2);
-      } else {
-        throw std::exception();
+        break;
       }
     } else {
       throw std::exception();
     }
   }
-
   if (s.size() != 1) {
     throw std::exception();
   }
-
   return s.top();
 }
